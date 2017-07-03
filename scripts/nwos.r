91a3c3982c0f8f5717bbf0efc06f656d1232ea73
rjzupkoii <rjzii@yahoo.com>
2017-07-03 13:59:15 -0400

    Updated script to create cumulative odds.

--- scripts/nwos.r
+++ scripts/nwos.r
@@ -12,25 +12,27 @@ loadData <- function(file) {
 	
 	# TODO Update everything to make use of tax data
 	raw = raw[, -c(3)]
-	
-	# Toss out all of the invalid ACT_CUT_SALE responses
+		
+	# Toss out all of the invalid ACT_CUT_SALE responses before removing the column 
 	raw = raw[raw$ACT_CUT_SALE != -1, ]
-	
+	raw = raw[, -c(2)]
+		
 	# Repalce parcel sizes with liguistic variables { small, medium, large }
 	raw$parcel <- NA
 	raw[raw$AC_WOOD < 40, ]$parcel = 'small'
 	raw[raw$AC_WOOD >= 100, ]$parcel = 'large'
 	raw[is.na(raw$parcel), ]$parcel = 'medium'
-	raw = raw[, -c(1)]							# Drop the AC_WOOD column
+	
+	# Drop the AC_WOOD column
+	raw = raw[, -c(1)]							
 	
 	# Update the column order and return
-	raw <- raw[, c(1, 2, 4, 3)]
 	return(raw)
 }
 
 process <- function(file) {
 	print(file)
-	
+			
 	# Load the data and note the index of the relevent variable
 	data = loadData(file)
 	name = strsplit(basename(file), '\\.')[[1]][1]
@@ -40,10 +42,30 @@ process <- function(file) {
 	data = data[data[index] > 0, ]				# Discard null responses
 	data[data[index] == 8, ][index] = 1			# Rescore NA as no concern
 	data = aggregate(Freq ~ ., data = data, sum)
+
+	# Find the odds to use for the data
+	data = findOdds(data)
 	
 	# Save the results
 	file = paste('../nwos/clean/', basename(file), sep = '')
 	write.csv(data, file, row.names = FALSE)
+}
+
+findOdds <- function(data) {
+	for (label in c('small', 'medium', 'large')) {
+		# Find the distributions for each group
+		total = sum(data[data$parcel == label, ]$Freq)
+		data[data$parcel == label, ]$Freq = data[data$parcel == label, ]$Freq / total
+		
+		# Make the distributions act as cummulative sums
+		for (index in c(2:5)) {
+			previous = data[data$parcel == label, ][index - 1, ]$Freq
+			value = data[data$parcel == label, ][index, ]$Freq
+			
+			data[data$parcel == label, ][index, ]$Freq = previous + value
+		}
+	}
+	return(data)
 }
 
 dir.create('../nwos/clean', showWarnings = FALSE)
@@ -65,4 +87,3 @@ process('../nwos/raw/OBJ_REC.csv')
 process('../nwos/raw/OBJ_TIM.csv')
 process('../nwos/raw/OBJ_WAT.csv')
 process('../nwos/raw/OBJ_WIL.csv')
-
